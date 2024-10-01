4ba6924c881863e9a6a58d34fd1532f17775c1c2
sheikh salman <salmanahmed@konasl.com>
2024-10-01 17:37:17 +0600

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors({
+    origin: '*', // Allow requests from any origin
+  });
   await app.listen(port).then(() => {
     console.log(`Server is running on http://localhost:${port}`);
   });
